6ae3b42e7a312e4e369827bd74fe87207e2c596c
dydes <942238839@qq.com>
2019-11-10 15:29:14 +0800

    11

--- 菜头食游记原始数据整理/1.商品明细表.vb
+++ 菜头食游记原始数据整理/1.商品明细表.vb
@@ -1,8 +1,5 @@
 Sub 数据整理()
 
-'省份还没清洗，如果区是空的就把前两列内容移过来，目前都是直辖市，有一些街道名得注意，有北京和北京市，得统一
-'原价得放到后面的表里一块处理
-
 Dim a As Integer
 
 '定位新数据截止行数
@@ -33,55 +30,9 @@ Dim a As Integer
         TextQualifier:=xlDoubleQuote, ConsecutiveDelimiter:=True, Tab:=False, _
         Semicolon:=False, Comma:=False, Space:=True, Other:=False, FieldInfo _
         :=Array(Array(1, 1), Array(2, 1)), TrailingMinusNumbers:=True
-
-'型号去重
-    '复制新增订单的商品信息
-    Range("H2:J" & a).Select
-    With Selection.Font
-        .Color = -16776961
-        .TintAndShade = 0
-    End With
-    With Selection.Interior
-        .Pattern = xlSolid
-        .PatternColorIndex = xlAutomatic
-        .Color = 65535
-        .TintAndShade = 0
-        .PatternTintAndShade = 0
-    End With
-    Range("H2:J" & a).Copy
-    '粘贴到商品信息表的末尾
-    Sheets("ProductCode").Select
-    rowmax1 = ActiveSheet.UsedRange.Rows.Count
-    Range("A" & rowmax1 + 1).Select
-    ActiveSheet.Paste
-    '商品信息去重
-    rowmax2 = ActiveSheet.UsedRange.Rows.Count
-    For i = rowmax1 + 1 To rowmax2
-        Range("D" & i) = Range("C" & i) & "||" & Range("B" & i)
-    Next
-    Columns("D:D").Select
-    Range("D" & rowmax1).Activate
-    ActiveSheet.Range("$A$1:$G$" & rowmax2).RemoveDuplicates Columns:=4, Header:=xlYes
-    
-'型号排序
-    rowmax3 = ActiveSheet.UsedRange.Rows.Count
-    Columns("A:G").Select
-    ActiveWorkbook.Worksheets("productCode").Sort.SortFields.Clear
-    ActiveWorkbook.Worksheets("productCode").Sort.SortFields.Add2 Key:=Range( _
-        "D2:D" & rowmax3), SortOn:=xlSortOnValues, Order:=xlAscending, DataOption:= _
-        xlSortNormal
-    With ActiveWorkbook.Worksheets("productCode").Sort
-        .SetRange Range("A1:G" & rowmax3)
-        .Header = xlYes
-        .MatchCase = False
-        .Orientation = xlTopToBottom
-        .SortMethod = xlPinYin
-        .Apply
-    End With
     
     MsgBox "完成"
 End Sub
-
 Sub 移动数据()
     rowmax = ActiveSheet.UsedRange.Rows.Count
     For i = 2 To rowmax
@@ -125,10 +76,8 @@ Sub 移动数据()
     Selection.Replace What:="重庆", Replacement:="重庆市", LookAt:=xlWhole, _
     SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
     ReplaceFormat:=False
+    
+    Range("W1") = "省"
         
         
 End Sub
-
-
-
-
