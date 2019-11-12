0921a20db2af52db0bcb1cd326e858bfc47ac8a6
dydes <942238839@qq.com>
2019-11-12 12:47:25 +0800

    1

---
+++ 课堂观察sql/新增教师.sql
@@ -0,0 +1,16 @@
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '胡艳','13957357949', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '沈龚','18768308633', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '沈炜','13586339020', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '朱亚楠','18329029813', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '王德胜','13515733970', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '缪钰','15990393757', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '屠大为','13362309287', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '彭衍军','13516734122', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '邢广柱','13867382548', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '沈涵','13967318207', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '刘玉生','15868369610', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '金凤芳','13758316252', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '朱轶沛','18358582862', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '张志英','13575311839', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '东健','13857382510', '0');
+INSERT INTO `htobservation`.`teacher_info` (`school_id`, `name`, `phone_num`, `is_deleted`) VALUES ('353', '罗玉芳','13757337349', '0');
---
+++ 课堂观察sql/新增用户.sql
@@ -0,0 +1,16 @@
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('胡艳','e10adc3949ba59abbe56e057f20f883e','13957357949', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('沈龚','e10adc3949ba59abbe56e057f20f883e','18768308633', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('沈炜','e10adc3949ba59abbe56e057f20f883e','13586339020', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('朱亚楠','e10adc3949ba59abbe56e057f20f883e','18329029813', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('王德胜','e10adc3949ba59abbe56e057f20f883e','13515733970', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('缪钰','e10adc3949ba59abbe56e057f20f883e','15990393757', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('屠大为','e10adc3949ba59abbe56e057f20f883e','13362309287', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('彭衍军','e10adc3949ba59abbe56e057f20f883e','13516734122', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('邢广柱','e10adc3949ba59abbe56e057f20f883e','13867382548', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('沈涵','e10adc3949ba59abbe56e057f20f883e','13967318207', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('刘玉生','e10adc3949ba59abbe56e057f20f883e','15868369610', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('金凤芳','e10adc3949ba59abbe56e057f20f883e','13758316252', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('朱轶沛','e10adc3949ba59abbe56e057f20f883e','18358582862', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('张志英','e10adc3949ba59abbe56e057f20f883e','13575311839', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('东健','e10adc3949ba59abbe56e057f20f883e','13857382510', '1', '0');
+INSERT INTO `htobservation`.`user_info` (`user_name`, `password`, `phone_num`, `is_active`, `is_deleted`) VALUES ('罗玉芳','e10adc3949ba59abbe56e057f20f883e','13757337349', '1', '0');
