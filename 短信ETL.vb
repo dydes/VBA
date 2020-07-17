f72c16594577e7da94c2684b5a0be795885a31e8
dydes <942238839@qq.com>
2020-07-17 15:08:50 +0800

    11

---
+++ 短信ETL.vb
@@ -0,0 +1,87 @@
+Sub 短信ETL()
+
+    Columns("D:E").Select
+    Selection.Delete Shift:=xlToLeft
+    
+    Columns("D:D").Select
+    Selection.Insert Shift:=xlToRight, CopyOrigin:=xlFormatFromLeftOrAbove
+    
+    Range("C1").Select
+    ActiveCell.FormulaR1C1 = "日期"
+    Range("D1").Select
+    ActiveCell.FormulaR1C1 = "时间"
+    
+    Range("C2").Select
+    Range(Selection, Selection.End(xlDown)).Select
+    Selection.TextToColumns Destination:=Range("C2"), DataType:=xlDelimited, _
+        TextQualifier:=xlDoubleQuote, ConsecutiveDelimiter:=True, Tab:=False, _
+        Semicolon:=False, Comma:=False, Space:=True, Other:=False, FieldInfo _
+        :=Array(Array(1, 1), Array(2, 1)), TrailingMinusNumbers:=True
+    
+    Columns("F:H").Select
+    Selection.Delete Shift:=xlToLeft
+
+
+    Columns("E:E").Select
+    Selection.Replace What:="*[修改密码]*", Replacement:="修改密码", LookAt:=xlPart, _
+        SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+
+    Selection.Replace What:="*[登录]*", Replacement:="登录", LookAt:=xlPart, _
+        SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+        
+    Selection.Replace What:="*[激活账号]*", Replacement:="激活账号", LookAt:=xlPart, _
+        SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+
+    Selection.Replace What:="*[完善家长手机号]*", Replacement:="完善家长手机号", LookAt:= _
+        xlPart, SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+        
+    Selection.Replace What:="*您有一份新的审题任务*", Replacement:="审题提示", LookAt:= _
+        xlPart, SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+
+    Selection.Replace What:="*[注册]*", Replacement:="注册", LookAt:=xlPart, _
+        SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+        
+    Selection.Replace What:="*[修改手机号]*", Replacement:="修改手机号", LookAt:=xlPart _
+        , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+
+    Selection.Replace What:="*无法提供录题服务*", Replacement:="无法录题通知", LookAt:= _
+        xlPart, SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+
+    Selection.Replace What:="*亲爱的会课学员*", Replacement:="直播课前提醒", LookAt:= _
+        xlPart, SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+        
+    Selection.Replace What:="*样本*", Replacement:="生成样本", LookAt:= _
+        xlPart, SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+        
+    Selection.Replace What:="*IP*", Replacement:="系统报错提示", LookAt:= _
+        xlPart, SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+        
+    Selection.Replace What:="*申请退款*", Replacement:="退款审核", LookAt:= _
+        xlPart, SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+        
+    Selection.Replace What:="*财务同意", Replacement:="同意退款", LookAt:= _
+        xlPart, SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+
+    Selection.Replace What:="*财务拒绝", Replacement:="拒绝退款", LookAt:= _
+        xlPart, SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+        
+
+    Selection.Replace What:="*[退款审核]*", Replacement:="退款审核", LookAt:= _
+        xlPart, SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
+        ReplaceFormat:=False
+
+End Sub
